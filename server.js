d2a648f6e05f6bcbc39e4e1a0f34eaf8a55477bc
spiritofiorentino <ken@spiritofiorentino.biz>
2025-02-24 21:45:05 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ 
+        status: "ok",
+        message: "NOWN API for Canva is active"
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
